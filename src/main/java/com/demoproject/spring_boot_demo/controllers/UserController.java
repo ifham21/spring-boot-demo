0548b5b225d9010fdd8adf98f5e06ef628f50dd2
Ahmed Ifham <ifham.maa@gmail.com>
2025-02-21 23:17:25 +0530

    Class

--- src/main/java/com/demoproject/spring_boot_demo/controllers/UserController.java
+++ src/main/java/com/demoproject/spring_boot_demo/controllers/UserController.java
@@ -1,4 +1,9 @@
 package com.demoproject.spring_boot_demo.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+//to change the class as a REST API controller, we have added @RestController
+@RestController
 public class UserController {
+    
 }
