6af1cfad61235f21f242999c761029a5fb6d8d9d
Ahmed Ifham <ifham.maa@gmail.com>
2025-02-21 23:42:29 +0530

    Changes

--- src/main/java/com/demoproject/spring_boot_demo/controllers/UserController.java
+++ src/main/java/com/demoproject/spring_boot_demo/controllers/UserController.java
@@ -1,16 +1,30 @@
 package com.demoproject.spring_boot_demo.controllers;
 
+import com.demoproject.spring_boot_demo.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 //to change the class as a REST API controller, we have added @RestController
 @RestController
 @RequestMapping("/api/users") // In order to get the request from a url we have used this annotation
 public class UserController {
 
+//    @GetMapping
+//    public String getUsers(){
+//        return "Hello Users";
+//    }
+
+
     @GetMapping
-    public String getUsers(){
-        return "Hello Users";
+    public List<User> getUsers(){
+        return Arrays.asList(
+                new User(1L, "John", "john@email.com"),
+                new User(2L, "Joe", "joe@email.com"),
+                new User(3L, "Alice", "alice@email.com")
+        );
     }
 }
