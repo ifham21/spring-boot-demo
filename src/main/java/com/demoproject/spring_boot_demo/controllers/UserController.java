2b73fef448f27c0f842a664decd8aba866a8af54
Ahmed Ifham <ifham.maa@gmail.com>
2025-02-21 23:25:00 +0530

    Used RequestMapping and GetMapping anotation and checked the response

--- src/main/java/com/demoproject/spring_boot_demo/controllers/UserController.java
+++ src/main/java/com/demoproject/spring_boot_demo/controllers/UserController.java
@@ -1,9 +1,16 @@
 package com.demoproject.spring_boot_demo.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 //to change the class as a REST API controller, we have added @RestController
 @RestController
+@RequestMapping("/api/users") // In order to get the request from a url we have used this annotation
 public class UserController {
-    
+
+    @GetMapping
+    public String getUsers(){
+        return "Hello Users";
+    }
 }
