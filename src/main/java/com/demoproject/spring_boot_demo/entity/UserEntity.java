d0852416bd2cb06d1cf4513b7e3c6b9a6e922b28
Ahmed Ifham <ifham.maa@gmail.com>
2025-02-22 23:22:59 +0530

    Changes

--- src/main/java/com/demoproject/spring_boot_demo/entity/UserEntity.java
+++ src/main/java/com/demoproject/spring_boot_demo/entity/UserEntity.java
@@ -1,4 +1,23 @@
 package com.demoproject.spring_boot_demo.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+    public UserEntity(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public UserEntity() {
+    }
 }
